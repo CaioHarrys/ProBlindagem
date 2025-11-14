64462d4725df542f7305d2cf0cdc8ec90a82dcfb
Caio Harrys <blackiaratanmusic@gmail.com>
2025-11-14 04:11:49 -0300

    Third Section
    Lados separados, cards novos feitos e estilizados. Faltando apenas o toque no UIUX

--- pro-blindagem/src/components/Sections/ThirdSection.jsx
+++ pro-blindagem/src/components/Sections/ThirdSection.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import ClientCard from '../ClientCard'
-import ImageComparisonSlider from '../ImageComparisonSlider'
+import React from "react";
+import ClientCard from "../ClientCard";
+import ImageComparisonSlider from "../ImageComparisonSlider";
 import "../../assets/css/ThirdSection.css";
+import MagicBento from "../MagicBento";
 
 const ThirdSection = () => {
   return (
-    <section className='third-section'>
-      <div className='container'>
-        <div className='left-side'>
-          <div className='left-side-content'>
+    <section className="third-section">
+      <div className="container">
+        <div className="left-side">
+          <div className="left-side-content">
             <ImageComparisonSlider />
           </div>
         </div>
-        <div className='right-side'>
-          <div className='right-side-content'>
-            <h1>Conteudo</h1>
+        <div className="right-side">
+          <div className="right-side-content">
+            <MagicBento
+              textAutoHide={true}
+              enableStars={true}
+              enableSpotlight={true}
+              enableBorderGlow={true}
+              enableTilt={true}
+              enableMagnetism={true}
+              clickEffect={true}
+              spotlightRadius={300}
+              particleCount={12}
+              glowColor="132, 0, 255"
+            />
           </div>
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default ThirdSection
+export default ThirdSection;
